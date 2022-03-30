7d4990480e3ad95a918569aabbf752172826f236
mahmoud44 <mrmahmoudadel1996@gmail.com>
2022-03-31 00:56:28 +0200

    adding-product-detail

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark">
+    <nav className="navbar navbar-expand-sm navbar-dark text-white">
       <div className="container-fluid">
         <Link className="navbar-brand" href="#" passHref>
-          Ecommerce App
+          <span className="text-white fw-bold">Ecommerce App</span>
         </Link>
         <button
           className="navbar-toggler"
@@ -19,7 +19,9 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="mynavbarr">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" href="#" passHref></Link>
+              <Link className="nav-link" href="#" passHref>
+                <></>
+              </Link>
             </li>
           </ul>
         </div>
